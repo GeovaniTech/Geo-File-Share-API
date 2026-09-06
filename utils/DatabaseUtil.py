429f79c3f11c4bc3e6a3a8c441ac803dfc173c1d
geovaniTech <geovanidebastiani32@gmail.com>
2026-09-06 17:58:24 -0300

    chore: Creating connection file
    Issue: https://github.com/GeovaniTech/Geo-File-Share-API/issues/2

---
+++ utils/DatabaseUtil.py
@@ -0,0 +1,22 @@
+import os
+import time
+import psycopg2
+
+def get_db_connection(retries=5, delay=2):
+    attempt = 0
+
+    while attempt < retries:
+        try:
+            attempt += 1
+            return psycopg2.connect(
+                database=os.getenv("DATABASE_NAME"),
+                host=os.getenv("DATABASE_HOST"),
+                user=os.getenv("DATABASE_USER"),
+                password=os.getenv("DATABASE_PASSWORD"),
+                port=os.getenv("DATABASE_PORT"),
+            )
+        except psycopg2.OperationalError as e:
+            if attempt == retries:
+                raise e
+            time.sleep(delay)
+    return None
